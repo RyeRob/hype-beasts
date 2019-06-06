c2c8b959e40a95503918d8d1c5201f0ec781e420
Ryan Robinson <ryan.robinson5@gmail.com>
2019-06-05 20:49:42 -0400

    stage 2

---
+++ src/app/mock-products.ts
@@ -0,0 +1,9 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+    {id:1, name:'Water Bottle', description:'drink stuff', price:100},
+    {id:2, name:'T-shirt', description: 'wear when its hot', price:150},
+    {id:3, name:'Longsleeve', description: 'wear it when its less hot', price: 200},
+    {id:4, name:'Hoodie', description: 'wear when its chilly', price: 300},
+    {id:5, name:'Socks', description:'put em on your feet', price:75},
+];
